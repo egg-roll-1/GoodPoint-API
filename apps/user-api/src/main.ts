4a224a056ce7a5f357bf958fcb25d43127f3e0ef
poby123 <i2pir@naver.com>
2025-04-14 19:52:58 +0900

    feat: swagger 문서 추가

--- apps/user-api/src/main.ts
+++ apps/user-api/src/main.ts
@@ -4,13 +4,34 @@ import {
 } from '@core/global/utils/init';
 import { NestFactory } from '@nestjs/core';
 import { UserAppModule } from './app.module';
+import { INestApplication } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   initializeBeforeApplication();
   const app = await NestFactory.create(UserAppModule);
   initializeApplication(app);
+  initializeSwagger(app);
 
   await app.listen(3000);
 }
+
+const initializeSwagger = (app: INestApplication) => {
+  const config = new DocumentBuilder()
+    .setTitle('Good Point User API')
+    .setDescription('일반 사용자를 위한 API 문서입니다.')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  document.paths = Object.keys(document.paths)
+    .reverse()
+    .reduce((acc, path) => {
+      acc[path] = document.paths[path];
+      return acc;
+    }, {});
+
+  SwaggerModule.setup('goodpoint-api-document', app, document);
+};
 
 bootstrap();

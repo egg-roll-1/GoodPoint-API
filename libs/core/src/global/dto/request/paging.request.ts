b1e7d4101b0ec9a1b71632e60ee0fb5501fbf226
poby123 <i2pir@naver.com>
2025-05-05 14:06:10 +0900

    feat: paging api

--- libs/core/src/global/dto/request/paging.request.ts
+++ libs/core/src/global/dto/request/paging.request.ts
@@ -1,3 +1,4 @@
+import { ApiPropertyOptional } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
 import { IsNumber, IsOptional } from 'class-validator';
 
@@ -5,11 +6,13 @@ export const defaultPageNumber: number = 1;
 export const defaultPageSize: number = 20;
 
 export class PagingRequest {
+  @ApiPropertyOptional({ description: '페이지', default: defaultPageNumber })
   @IsOptional()
   @IsNumber()
   @Type(() => Number)
   page?: number = defaultPageNumber;
 
+  @ApiPropertyOptional({ description: '크기', default: defaultPageSize })
   @IsOptional()
   @IsNumber()
   @Type(() => Number)

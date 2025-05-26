9bf55700dc7869e0c1ce1314123390ac94d418dd
andy1398 <byeong3018@gmail.com>
2025-05-26 17:48:47 +0900

    new folder

--- apps/user-api/src/order/service/order.service.ts
+++ apps/user-api/src/order/service/order.service.ts
@@ -4,4 +4,9 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class OrderService {
   constructor(private orderRepository: OrderRepository) {}
+  // 주문 요청을 받아서,
+  // 주문
+  // 1. 상품이 유효한지  (사용자 유효 )
+  // 2. 사용자 잔액이 있는지 ( 잔액 크래딧딧)
+  // 3. 주문을 만들어서 저장 (orderItem, creditHistory)
 }

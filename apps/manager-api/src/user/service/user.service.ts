21d6751625cb654777f7c38a490770442a11be2c
poby123 <i2pir@naver.com>
2025-05-26 15:39:16 +0900

    feat: manager user api

--- apps/manager-api/src/user/service/user.service.ts
+++ apps/manager-api/src/user/service/user.service.ts
@@ -3,10 +3,19 @@ import { UserException } from '@core/domain/user/exception/user.exception';
 import { AsyncTimeLogger } from '@core/global/decorator/time.decorator';
 import { EGException } from '@core/global/exception/exception';
 import { Injectable } from '@nestjs/common';
+import { Transactional } from 'typeorm-transactional';
+import { PatchManagerRequest } from '../dto/request/user.request';
 
 @Injectable()
 export class ManagerService {
   constructor(private managerRepository: ManagerRepository) {}
+
+  @AsyncTimeLogger()
+  @Transactional()
+  async patchUserProfile(userId: number, request: PatchManagerRequest) {
+    const manager = await this.getUserProfile(userId);
+    await this.managerRepository.update(manager.id, request.toEntity(manager));
+  }
 
   @AsyncTimeLogger()
   async getUserProfile(userId: number) {
